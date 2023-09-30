676ad5093bd852f84a712dd40f176764fc4e22ac
Esmeldy <67498436+Esmeldy@users.noreply.github.com>
2023-09-30 18:50:08 +0200

    Añadida la implementación de CatalogoPeliculas

---
+++ src/main/java/peliculas/negocio/CatalogoPeliculasImp.java
@@ -0,0 +1,23 @@
+package peliculas.negocio;
+
+public class CatalogoPeliculasImp implements ICatalogoPeliculas{
+    @Override
+    public void agregarPelicula(String nombrePelicula) {
+
+    }
+
+    @Override
+    public void listarPelicula() {
+
+    }
+
+    @Override
+    public void buscarPelicula(String nombrePelicula) {
+
+    }
+
+    @Override
+    public void iniciarCatalogoPeliculas() {
+
+    }
+}
